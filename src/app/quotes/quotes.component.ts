951febda144c487335baec6d6fe646bdedeae9c5
Joy Marocho <joy.marocho@student.moringaschool.com>
2022-04-10 15:01:43 +0300

    create html and update its details

--- src/app/quotes/quotes.component.ts
+++ src/app/quotes/quotes.component.ts
@@ -9,22 +9,37 @@ import { Quote } from '../quote';
 export class QuotesComponent implements OnInit {
 
   quotes: Quote[] = [
-    new Quote (1, 'Darkness cannot drive out darkness: only light can do that. Hate cannot drive out hate: only love can do that.', 'Martin Luther King', 'Dennis Njeru', new Date(2020, 4, 20)),
-    new Quote (2, 'There are only two ways to live your life. One is as though nothing is a miracle. The other is as though everything is a miracle.', 'Albert Einstein', 'Eric Nzioka', new Date(2020, 7, 25)),
-    new Quote (3, 'You have brains in your head. You have feet in your shoes. You can steer yourself any direction you choose. You are on your own. And you know what you know. And YOU are the one who will decide where to go.', 'Dr. Seuss', 'Ian Nzioka', new Date(2021, 2, 10)),
-    new Quote (4, 'There is no greater agony than bearing an untold story inside you.', 'Maya Angelou', 'Ann Boohle', new Date(2021, 4, 21)),
-    new Quote (5, 'When one door of happiness closes, another opens; but often we look so long at the closed door that we do not see the one which has been opened for us.', 'Helen Keller', 'Vivian Saumu', new Date(2021, 6, 31)),
+    new Quote (1, 'Do you want to know who you are. Do not ask. Act. Action will delineate and define you.', 'Thomas Jefferson', 'Joy Marocho', new Date(2022, 5, 10)), 
+    new Quote (2, 'There are only two ways to live your life. One is as though nothing is a miracle. The other is as though everything is a miracle.', 'Albert Einstein', 'Astrid Keonna', new Date(2022, 6, 27)),
+    new Quote (3, 'Leave all the afternoon for exercise and recreation, which are as necessary as reading. I will rather say more necessary because health is worth more than learning.', 'Thomas Jefferson', 'Jade Varsquez', new Date(2020, 12, 08)),
+    new Quote (4, 'f you can not fly then run, if you can not run then walk, if you can not walk then crawl, but whatever you do you have to keep moving forward.', 'Martin Luther King, Jr', 'Hope Nkatha', new Date(2021, 3, 16)),
+    new Quote (5, 'Don not worry about people stealing your design work. Worry about the day they stop.', 'Jeffrey Zeldman', 'Gloria Kawira', new Date(2021, 6, 22)),
+    new Quote (2, 'Chase the vision, not the money, the money will end up following you.', 'Tony Hsieh', 'Don Caleb', new Date(2019, 1, 25)),
   ];
 
   displayDetails(index: any){
     this.quotes[index].showAuthorAndSubmitter = !this.quotes[index].showAuthorAndSubmitter;
   }
 
-
-
-  constructor() { }
-
-  ngOnInit(): void {
+  addNewQuote(quote:any){
+    let quoteLength = this.quotes.length;
+    quote.id = quoteLength+1;
+    quote.dateEntered = new Date(quote.dateEntered)
+    this.quotes.push(quote)
   }
+
+  deleteQuote(isComplete:any, index:any){
+    if(isComplete){
+      let toDelete = confirm('Are you sure you want to delete a quote by ${this.quotes[index].author}?')
+    
+      if(toDelete){
+        this.quotes.splice(index, 1);
+      }
+    }
+  }
+
+  constructor() {}
+
+  ngOnInit(): void {}
 
 }
