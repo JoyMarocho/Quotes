5fccda753a04bcd7e89576684f6b228804904174
Joy Marocho <joy.marocho@student.moringaschool.com>
2022-04-12 14:57:23 +0300

    Auto-generated commit [ci skip]

--- src/app/highlightghighvotequote.directive.spec.ts
+++
@@ -1,8 +0,0 @@
-import { HighlightghighvotequoteDirective } from './highlightghighvotequote.directive';
-
-describe('HighlightghighvotequoteDirective', () => {
-  it('should create an instance', () => {
-    const directive = new HighlightghighvotequoteDirective();
-    expect(directive).toBeTruthy();
-  });
-});
--- src/app/quote.ts
+++
@@ -1,6 +0,0 @@
-export class Quote {
-    showAuthorAndSubmitter: boolean;
-    constructor(public id: number, public quote: string, public author: string, public submitter: string, public dateEntered: Date){
-    this.showAuthorAndSubmitter=false;
-    }
-}
